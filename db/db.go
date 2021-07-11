7c5bd6bf7ec03bb22938b9940dbccc09dece6e5a
hawwwdi <hawwwdi@gmail.com>
2021-07-11 23:16:19 +0430

    db methods Done

--- db/db.go
+++ db/db.go
@@ -42,18 +42,6 @@ func (db *DB) Login(l *model.LoginCredential) error {
 	return err
 }
 
-func (db *DB) PostAva(a *model.Ava) error {
-	query := `call postAVA_procedure(?)`
-	_, err := db.SqlDB.Exec(query, a.Content)
-	return err
-}
-
-func (db *DB) PostComment(a *model.Ava) error {
-	query := `call postComment_procedure(?, ?)`
-	_, err := db.SqlDB.Exec(query, a.Content, a.ReplyTo)
-	return err
-}
-
 func (db *DB) Follow(u *model.User) error {
 	query := `call follow(?)`
 	res, err := db.SqlDB.Exec(query, u.Username)
@@ -111,6 +99,54 @@ func (db *DB) SendMessage(m *model.Message) error {
 	if rows == 0 {
 		return fmt.Errorf("message not send")
 	}
+	return err
+}
+
+func (db *DB) GetMessagesFromAUser(u *model.User) ([]*model.Message, error) {
+	query := `call getMessagesFromAUser(?)`
+	rows, err := db.SqlDB.Query(query, u.Username)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	messages := make([]*model.Message, 0)
+	for rows.Next() {
+		var curr model.Message
+		if err := rows.Scan(&curr.ID, &curr.Sender, &curr.Receiver, &curr.Content, &curr.PostID, &curr.SendTime); err != nil {
+			return nil, err
+		}
+		messages = append(messages, &curr)
+	}
+	return messages, nil
+}
+
+func (db *DB) GetMessageSenders() ([]*model.User, error) {
+	query := `call getSenders()`
+	rows, err := db.SqlDB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	senders := make([]*model.User, 0)
+	for rows.Next() {
+		var curr model.User
+		if err := rows.Scan(&curr.ID, &curr.Username); err != nil {
+			return nil, err
+		}
+		senders = append(senders, &curr)
+	}
+	return senders, nil
+}
+
+func (db *DB) PostAva(a *model.Ava) error {
+	query := `call postAVA_procedure(?)`
+	_, err := db.SqlDB.Exec(query, a.Content)
+	return err
+}
+
+func (db *DB) PostComment(a *model.Ava) error {
+	query := `call postComment_procedure(?, ?)`
+	_, err := db.SqlDB.Exec(query, a.Content, a.ReplyTo)
 	return err
 }
 
