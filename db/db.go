67caffb8eede684f2b562d89c5e695a441fd4d20
hawwwdi <hawwwdi@gmail.com>
2021-07-11 23:06:40 +0430

    add getAvasByTag and getUserAvas methods

--- db/db.go
+++ db/db.go
@@ -166,7 +166,42 @@ func (db *DB) GetPersonalAvas() ([]*model.Ava, error) {
 		timeLine = append(timeLine, &curr)
 	}
 	return timeLine, nil
+}
 
+func (db *DB) GetUserAvas(u *model.User) ([]*model.Ava, error) {
+	query := `call getUserAvas_procedure(?)`
+	rows, err := db.SqlDB.Query(query, u.Username)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	timeLine := make([]*model.Ava, 0)
+	for rows.Next() {
+		var curr model.Ava
+		if err := rows.Scan(&curr.ID, &curr.Publisher, &curr.Content, &curr.ReplyTo, &curr.PublishTime); err != nil {
+			return nil, err
+		}
+		timeLine = append(timeLine, &curr)
+	}
+	return timeLine, nil
+}
+
+func (db *DB) GetAvasByTags(tag string) ([]*model.Ava, error) {
+	query := `call getPostByTag_procedure(?)`
+	rows, err := db.SqlDB.Query(query, tag)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	timeLine := make([]*model.Ava, 0)
+	for rows.Next() {
+		var curr model.Ava
+		if err := rows.Scan(&curr.ID, &curr.Publisher, &curr.Content, &curr.ReplyTo, &curr.PublishTime); err != nil {
+			return nil, err
+		}
+		timeLine = append(timeLine, &curr)
+	}
+	return timeLine, nil
 }
 
 func (db *DB) GetAvasByLikes() ([]*model.Ava, error) {
