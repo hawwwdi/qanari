d6dac822ed207f6c6206062235e1775d91837609
hawwwdi <hawwwdi@gmail.com>
2021-07-12 17:19:44 +0430

    add marshal and unmarshal method for message struct

--- model/message.go
+++ model/message.go
@@ -1,10 +1,56 @@
 package model
 
+import (
+	"database/sql"
+	"encoding/json"
+)
+
 type Message struct {
-	ID       int    `json:"id" binding:"-"`
-	Sender   int    `json:"sender" binding:"-"`
-	Receiver int    `json:"receiver" binding:"required"`
-	Content  string `json:"content" binding:"-"`
-	PostID   int    `json:"postID" binding:"-"`
-	SendTime string `json:"sendTime" binding:"-"`
+	ID       int            `json:"id" binding:"-"`
+	Sender   int            `json:"sender" binding:"-"`
+	Receiver string         `json:"receiver" binding:"required"`
+	Content  sql.NullString `json:"content" binding:"-"`
+	PostID   sql.NullInt32  `json:"postID" binding:"-"`
+	SendTime string         `json:"sendTime" binding:"-"`
+}
+
+func (m Message) MarshalJSON() ([]byte, error) {
+	res := make(map[string]interface{})
+	res["id"] = m.ID
+	res["sender"] = m.Sender
+	res["receiver"] = m.Receiver
+	res["sendTime"] = m.SendTime
+	if m.Content.Valid {
+		res["content"] = m.Content.String
+	}
+	if m.PostID.Valid {
+		res["postID"] = m.PostID.Int32
+	}
+	return json.Marshal(res)
+}
+
+func (m *Message) UnmarshalJSON(b []byte) error {
+	res := make(map[string]interface{})
+	_ = json.Unmarshal(b, &res)
+	if _, valid := res["id"]; valid {
+		m.ID = res["id"].(int)
+	}
+	if _, valid := res["sender"]; valid {
+		m.Sender = res["sender"].(int)
+	}
+	if _, valid := res["receiver"]; valid {
+		m.Receiver = res["receiver"].(string)
+	}
+	if _, valid := res["sendTime"]; valid {
+		m.SendTime = res["sendTime"].(string)
+	}
+	if _, valid := res["content"]; valid {
+		m.Content.String = res["content"].(string)
+		m.Content.Valid = true
+	}
+	if _, valid := res["postID"]; valid {
+		m.PostID.Int32 = int32(res["postID"].(float64))
+		m.Content.Valid = true
+	}
+	return nil
 }
