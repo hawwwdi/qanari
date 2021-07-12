dd7c4017a0a12ae47575c0e90b20036387a375b2
hawwwdi <hawwwdi@gmail.com>
2021-07-12 18:48:58 +0430

    update addTagToPost function

--- db/sql_quries/addTagsToPost.sql
+++ db/sql_quries/addTagsToPost.sql
@@ -5,15 +5,16 @@ insert into ava_tag(AID, TID)
 values (1 ,(select tag.ID from tag where tag.hashtag = '#hadi'));
 
 
-create procedure addTagToAPost(IN PAID int, IN pTag varchar(6))
+create
+    definer = root@`%` procedure addTagToAPost(IN PAID int, IN pTag varchar(6))
 begin
     declare res INT default NULL;
     select tag.ID into res from tag
         where tag.hashtag = pTag;
-    IF res = NULL then
-        insert into tag values (pTag);
+    IF res is NULL then
+        insert into tag(hashtag) values (pTag);
         select tag.ID into res from tag
         where tag.hashtag = pTag;
     end if;
     insert into ava_tag values (PAID, res);
-end ;
+end;
