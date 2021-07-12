9059ad73f7ab5367fc76b75cf6179fa02c9496f6
hawwwdi <hawwwdi@gmail.com>
2021-07-12 18:28:34 +0430

    add umarshal and marshal method for ava model

--- model/ava.go
+++ model/ava.go
@@ -1,10 +1,53 @@
 package model
 
+import (
+	"database/sql"
+	"encoding/json"
+)
+
 type Ava struct {
-	ID          int64  `json:"id" binding:"-"`
-	Publisher   int64  `json:"publisher" binding:"-"`
-	Content     string `json:"content" binding:"-"`
-	ReplyTo     int64  `json:"replyTo" binding:"-"`
-	PublishTime string `json:"publishTime" binding:"-"`
-	Likes       int    `json:"likes" binding:"-"`
+	ID          int           `json:"id" binding:"-"`
+	Publisher   int           `json:"publisher" binding:"-"`
+	Content     string        `json:"content" binding:"-"`
+	ReplyTo     sql.NullInt32 `json:"replyTo" binding:"-"`
+	PublishTime string        `json:"publishTime" binding:"-"`
+	Likes       int           `json:"likes" binding:"-"`
+}
+
+func (a Ava) MarshalJSON() ([]byte, error) {
+	res := make(map[string]interface{})
+	res["id"] = a.ID
+	res["publisher"] = a.Publisher
+	res["content"] = a.Content
+	res["publishTime"] = a.PublishTime
+	res["likes"] = a.Likes
+	if a.ReplyTo.Valid {
+		res["replyTo"] = a.ReplyTo.Int32
+	}
+	return json.Marshal(res)
+}
+
+func (a *Ava) UnmarshalJSON(b []byte) error {
+	res := make(map[string]interface{})
+	_ = json.Unmarshal(b, &res)
+	if _, valid := res["id"]; valid {
+		a.ID = res["id"].(int)
+	}
+	if _, valid := res["publisher"]; valid {
+		a.Publisher = res["publisher"].(int)
+	}
+	if _, valid := res["content"]; valid {
+		a.Content = res["content"].(string)
+	}
+	if _, valid := res["publishTime"]; valid {
+		a.PublishTime = res["publishTime"].(string)
+	}
+	if _, valid := res["likes"]; valid {
+		a.Likes = res["likes"].(int)
+	}
+	if _, valid := res["replyTo"]; valid {
+		a.ReplyTo.Int32 = int32(res["postID"].(float64))
+		a.ReplyTo.Valid = true
+	}
+	return nil
 }
