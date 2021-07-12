743c20660cff2cea196155946f3c979dcc078e09
hawwwdi <hawwwdi@gmail.com>
2021-07-12 15:11:02 +0430

    message handlers added

--- db/message.go
+++ db/message.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hawwwdi/qanari/model"
 )
 
-func (db *DB) SendMessage(m *model.Message) error {
+func (db *DB) SendMessage(m model.Message) error {
 	query := `call sendMessage(?, ?, ?)`
 	res, err := db.SqlDB.Exec(query, m.Receiver, m.Content, m.PostID)
 	rows, _ := res.RowsAffected()
@@ -16,38 +16,38 @@ func (db *DB) SendMessage(m *model.Message) error {
 	return err
 }
 
-func (db *DB) GetMessagesFromAUser(u *model.User) ([]*model.Message, error) {
+func (db *DB) GetMessagesFromAUser(u model.User) ([]model.Message, error) {
 	query := `call getMessagesFromAUser(?)`
 	rows, err := db.SqlDB.Query(query, u.Username)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	messages := make([]*model.Message, 0)
+	messages := make([]model.Message, 0)
 	for rows.Next() {
 		var curr model.Message
 		if err := rows.Scan(&curr.ID, &curr.Sender, &curr.Receiver, &curr.Content, &curr.PostID, &curr.SendTime); err != nil {
 			return nil, err
 		}
-		messages = append(messages, &curr)
+		messages = append(messages, curr)
 	}
 	return messages, nil
 }
 
-func (db *DB) GetMessageSenders() ([]*model.User, error) {
+func (db *DB) GetMessageSenders() ([]model.User, error) {
 	query := `call getSenders()`
 	rows, err := db.SqlDB.Query(query)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	senders := make([]*model.User, 0)
+	senders := make([]model.User, 0)
 	for rows.Next() {
 		var curr model.User
 		if err := rows.Scan(&curr.ID, &curr.Username); err != nil {
 			return nil, err
 		}
-		senders = append(senders, &curr)
+		senders = append(senders, curr)
 	}
 	return senders, nil
 }
---
+++ handlers/message.go
@@ -0,0 +1,84 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hawwwdi/qanari/db"
+	"github.com/hawwwdi/qanari/model"
+)
+
+func getSendMessageHandler(db *db.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var msg model.Message
+		if err := c.ShouldBindJSON(&msg); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		if err := db.SendMessage(msg); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "message was sent successfully",
+		})
+		return
+	}
+}
+
+func getMessagesFromAUserHandler(db *db.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user model.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		messages, err := db.GetMessagesFromAUser(user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		bytes, err := json.Marshal(messages)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"messages": string(bytes),
+		})
+	}
+}
+
+func getMessageSendersHandler(db *db.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		users, err := db.GetMessageSenders()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		bytes, err := json.Marshal(users)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"senders": string(bytes),
+		})
+		return
+	}
+}
